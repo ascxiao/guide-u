e9d224616b72dd004bbeb4ae373ffab2d0f0b9a8
JohnEsperancilla-PH <business.jlte@gmail.com>
2026-03-17 22:55:39 +0800

    FEAT: Google OAuth Integration with Sign in with Google

--- frontend/guide_u_flutter/lib/routes/app_routes.dart
+++ frontend/guide_u_flutter/lib/routes/app_routes.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../views/handbook_main_page.dart';
 import '../views/handbook_article_page.dart';
 import '../views/handbook_saved_articles_page.dart';
 import '../views/handbook_search_screen.dart';
 import '../views/handbook_chatbot.dart';
+import '../views/login_screen.dart';
 
 class AppRoutes {
   static const String main = '/';
+  static const String login = '/login';
   static const String article = '/article';
   static const String saved = '/saved';
   static const String search = '/search';
   static const String chatbot = '/chatbot';
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
+    final hasSession = Supabase.instance.client.auth.currentSession != null;
+    final routeName = settings.name ?? main;
+
+    if (!hasSession && routeName != login) {
+      return MaterialPageRoute(
+        builder: (_) => const LoginScreen(),
+        settings: const RouteSettings(name: login),
+      );
+    }
+
+    if (hasSession && routeName == login) {
+      return MaterialPageRoute(
+        builder: (_) => const HandbookMainPage(),
+        settings: const RouteSettings(name: main),
+      );
+    }
+
+    switch (routeName) {
+      case login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       case main:
-        return MaterialPageRoute(
-          builder: (_) => const HandbookMainPage(),
-        );
+        return MaterialPageRoute(builder: (_) => const HandbookMainPage());
       case article:
         final args = settings.arguments as Map<String, dynamic>?;
         return MaterialPageRoute(
@@ -32,13 +52,9 @@ class AppRoutes {
           builder: (_) => const HandbookSavedArticlesPage(),
         );
       case search:
-        return MaterialPageRoute(
-          builder: (_) => const HandbookSearchScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => const HandbookSearchScreen());
       case chatbot:
-        return MaterialPageRoute(
-          builder: (_) => const HandbookChatbotScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => const HandbookChatbotScreen());
       default:
         return null;
     }
