c8f68243470d85ba8e707393677f6dd2889e2f60
ysa <alyssamagay03@gmail.com>
2026-03-25 16:29:39 +0800

    FIX: Fixed conflics in form submission pages

--- frontend/guide_u_flutter/lib/views/handbook_lost_and_found_page.dart
+++ frontend/guide_u_flutter/lib/views/handbook_lost_and_found_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'handbook_bottom_nav_bar.dart';
 import '../widgets/internet_required_notice.dart';
-import '../widgets/handbook_chatbot_fab.dart';
 import '../view_models/lost_found_report_view_model.dart';
 
 class HandbookLostAndFoundPage extends StatefulWidget {
@@ -512,9 +511,6 @@ class _HandbookLostAndFoundPageState extends State<HandbookLostAndFoundPage> {
           );
         },
       ),
-      bottomNavigationBar: const HandbookBottomNavBar(currentIndex: 1),
-      floatingActionButton: const HandbookChatbotFAB(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
